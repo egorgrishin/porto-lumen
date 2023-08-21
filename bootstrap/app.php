d56b97c43d18cfb15191930b8bed0e8f7625d39d
Egor Grishin <egor-grishin@bk.ru>
2023-08-21 19:48:29 +0500

    bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,15 @@
 <?php
 
+use Core\Exceptions\Handler;
+use Core\Kernels\Kernel;
+use Illuminate\Contracts\Console\Kernel as LumenKernel;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Laravel\Lumen\Application;
+use Laravel\Lumen\Bootstrap\LoadEnvironmentVariables;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
+(new LoadEnvironmentVariables(
     dirname(__DIR__)
 ))->bootstrap();
 
@@ -19,9 +26,7 @@ date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 |
 */
 
-$app = new Laravel\Lumen\Application(
-    dirname(__DIR__)
-);
+$app = new Application(dirname(__DIR__));
 
 // $app->withFacades();
 
@@ -34,19 +39,12 @@ $app = new Laravel\Lumen\Application(
 |
 | Now we will register a few bindings in the service container. We will
 | register the exception handler and the console kernel. You may add
-| your own bindings here if you like or you can make another file.
+| your own bindings here if you like, or you can make another file.
 |
 */
 
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    \Core\Exceptions\Handler::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    \Core\Kernels\Kernel::class
-);
+$app->singleton(ExceptionHandler::class, Handler::class);
+$app->singleton(LumenKernel::class, Kernel::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -85,7 +83,7 @@ $app->configure('app');
 | Register Service Providers
 |--------------------------------------------------------------------------
 |
-| Here we will register all of the application's service providers which
+| Here we will register all the application's service providers which
 | are used to bind services into the container. Service providers are
 | totally optional, so you are not required to uncomment this line.
 |
@@ -94,22 +92,5 @@ $app->configure('app');
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-
-/*
-|--------------------------------------------------------------------------
-| Load The Application Routes
-|--------------------------------------------------------------------------
-|
-| Next we will include the routes file so that they can all be added to
-| the application. This will provide all of the URLs the application
-| can respond to, as well as the controllers that may handle them.
-|
-*/
-
-$app->router->group([
-    'namespace' => 'App\Http\Controllers',
-], function ($router) {
-    require __DIR__.'/../routes/web.php';
-});
 
 return $app;
