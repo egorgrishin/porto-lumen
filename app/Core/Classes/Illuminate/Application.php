4322d7ced5224c3840a263e970ce122531abefcc
Egor Grishin <egor-grishin@bk.ru>
2023-08-29 03:26:32 +0500

    application get resolved dependencies method

--- app/Core/Classes/Illuminate/Application.php
+++ app/Core/Classes/Illuminate/Application.php
@@ -3,9 +3,12 @@
 namespace Core\Classes\Illuminate;
 
 use Core\Classes\Illuminate\Concerns\RoutesRequests;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Laravel\Lumen\Application as LumenApplication;
 use Laravel\Lumen\Routing\Router;
+use ReflectionClass;
+use ReflectionException;
 
 class Application extends LumenApplication
 {
@@ -49,5 +52,52 @@ class Application extends LumenApplication
     public function corePath(string $path = ''): string
     {
         return $this->appPath() . DIRECTORY_SEPARATOR . 'Core' . ($path ? DIRECTORY_SEPARATOR . $path : '');
+    }
+
+    /**
+     * Returns resolved dependencies
+     * @throws BindingResolutionException
+     */
+    public function getResolvedDependencies(string $abstract): array
+    {
+        try {
+            $reflector = new ReflectionClass($abstract);
+        } catch (ReflectionException $exception) {
+            throw new BindingResolutionException(
+                "Target class [$abstract] does not exist.",
+                0,
+                $exception
+            );
+        }
+
+        // If the type is not instantiable, the developer is attempting to resolve
+        // an abstract type such as an Interface or Abstract Class and there is
+        // no binding registered for the abstractions, so we need to bail out.
+        if (!$reflector->isInstantiable()) {
+            $this->notInstantiable($abstract);
+        }
+
+        // If there are no constructors, that means there are no dependencies then
+        // we can just resolve the instances of the objects right away, without
+        // resolving any other types or dependencies out of these containers.
+        if (is_null($constructor = $reflector->getConstructor())) {
+            return [];
+        }
+
+        $this->buildStack[] = $abstract;
+        $dependencies = $constructor->getParameters();
+
+        // Once we have all the constructor's parameters we can create each of the
+        // dependency instances and then use the reflection instances to make a
+        // new instance of this class, injecting the created dependencies in.
+        try {
+            $instances = $this->resolveDependencies($dependencies);
+        } catch (BindingResolutionException $exception) {
+            array_pop($this->buildStack);
+            throw $exception;
+        }
+
+        array_pop($this->buildStack);
+        return $instances;
     }
 }
