19127e8716cec7a0061d2b36ab67f9ff6fabc001
Egor Grishin <egor-grishin@bk.ru>
2023-08-28 06:10:10 +0500

    controller action method - first version

--- app/Core/Parents/BaseController.php
+++ app/Core/Parents/BaseController.php
@@ -4,5 +4,11 @@ namespace Core\Parents;
 
 abstract class BaseController
 {
-    //
+    /**
+     * Returns an object of the BaseAction class
+     */
+    public function action(string $abstract): BaseAction
+    {
+        return new $abstract;
+    }
 }
