fa92a3f6fef7b18a36b57a273b19d04491b1dfe7
Egor Grishin <egor-grishin@bk.ru>
2023-08-28 06:10:41 +0500

    user routes

--- app/Sections/User/User/Routes/rest.php
+++ app/Sections/User/User/Routes/rest.php
@@ -1,6 +1,11 @@
 <?php
 
-use Laravel\Lumen\Routing\Router;
-
 /** @var Router $router */
-$router->get('/', fn () => dd(config('app')));
+use Laravel\Lumen\Routing\Router;
+use Sections\User\User\Controllers\UserController;
+
+$router->group(['prefix' => 'v1'], function () use ($router) {
+    $router->post('/users', [UserController::class, 'create']);
+    $router->patch('/users/{id}', [UserController::class, 'update']);
+    $router->delete('/users/{id}', [UserController::class, 'delete']);
+});
