5f656938115ee4c9217cbdd4fd2607375744f072
Egor Grishin <egor-grishin@bk.ru>
2023-08-28 06:03:02 +0500

    settings base test class

--- app/Core/Parents/BaseTest.php
+++ app/Core/Parents/BaseTest.php
@@ -3,15 +3,27 @@
 namespace Core\Parents;
 
 use Laravel\Lumen\Application;
+use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\TestCase;
 
 abstract class BaseTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * Creates the application.
      */
     public function createApplication(): Application
     {
         return require __DIR__ . '/../../../bootstrap/app.php';
+    }
+
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('db:seed');
     }
 }
