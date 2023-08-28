fd782b6f3291bb1cc8c1d947e6ac980f7c451847
Egor Grishin <egor-grishin@bk.ru>
2023-08-28 15:44:41 +0500

    add base action task method - version 1

--- app/Core/Parents/BaseAction.php
+++ app/Core/Parents/BaseAction.php
@@ -4,5 +4,11 @@ namespace Core\Parents;
 
 abstract class BaseAction
 {
-    //
+    /**
+     * Returns an object of the BaseTask class
+     */
+    public function task(string $abstract): BaseTask
+    {
+        return new $abstract;
+    }
 }
