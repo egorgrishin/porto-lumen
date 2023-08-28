0dd77a2e11df058279cab43eb2976166377c27aa
Egor Grishin <egor-grishin@bk.ru>
2023-08-29 03:40:22 +0500

    fix client resource

--- app/Sections/User/Client/Resources/ClientResource.php
+++ app/Sections/User/Client/Resources/ClientResource.php
@@ -22,7 +22,7 @@ class ClientResource extends BaseResource
             'id'         => $this->when(isset($client->id), fn () => $client->id),
             'name'       => $this->when(isset($client->name), fn () => $client->name),
             'email'      => $this->when(isset($client->email), fn () => $client->email),
-            'role_type'  => $this->when(isset($client->role_type), fn () => $client->role),
+            'role_type'  => $this->when(isset($client->role_type), fn () => $client->role_type),
             'created_at' => $this->when(isset($client->created_at), fn () => $client->created_at),
             'updated_at' => $this->when(isset($client->updated_at), fn () => $client->updated_at),
             'deleted_at' => $this->when(isset($client->deleted_at), fn () => $client->deleted_at),
